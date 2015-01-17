4558e0ee2a51adceed887b3d7fda56fae778e0a3
VincentLoy <vincent.loy1@gmail.com>
2015-01-17 19:58:26 +0100

    remove useless code

--- viewmodels/modalViewModel.js
+++ viewmodels/modalViewModel.js
@@ -1,56 +1,10 @@
 define(function(require, exports, module){
     var ko = require('../vendor/knockout'),
         _ = require('../vendor/lodash'),
-        fs = require('../services/filesystem'),
-        ExtensionUtils = brackets.getModule("utils/ExtensionUtils"),
-        NodeConnection = brackets.getModule("utils/NodeConnection"),
-        DocumentManager = brackets.getModule("document/DocumentManager"),
-        EditorManager = brackets.getModule("editor/EditorManager"),
-        nodeConnection = new NodeConnection();
-
-    function chain() {
-        var functions = Array.prototype.slice.call(arguments, 0);
-        if (functions.length > 0) {
-            var firstFunction = functions.shift();
-            var firstPromise = firstFunction.call();
-            firstPromise.done(function () {
-                chain.apply(null, functions);
-            });
-        }
-    }
-
-    function connect() {
-        var connectionPromise = nodeConnection.connect(true);
-        connectionPromise.fail(function () {
-            console.error("[brackets-simple-node] failed to connect to node");
-        });
-        return connectionPromise;
-    }
-
-
-    function loadClipboard() {
-        var path = ExtensionUtils.getModulePath(module, "node/clipboard");
-        var loadPromise = nodeConnection.loadDomains([path], true);
-        loadPromise.fail(function (e) {
-            console.log(e);
-            console.log("[brackets-simple-node] failed to load clipboard");
-        });
-        return loadPromise;
-    }
-
-
-    function clipboardLoad() {
-        var loadPromise = nodeConnection.domains.clipboard.load();
-        loadPromise.fail(function (err) {
-            console.error("[brackets-simple-node] failed to run clipboard.load", err);
-        });
-        loadPromise.done(function (err) {
-            //loaded
-
-        });
-        return loadPromise;
-    }
-    //chain(connect, loadClipboard, clipboardLoad);
+        NodeConnection = brackets.getModule("utils/NodeConnection"), //required
+        DocumentManager = brackets.getModule("document/DocumentManager"), //required
+        EditorManager = brackets.getModule("editor/EditorManager"), //required
+        nodeConnection = new NodeConnection(); //required
 
     function ModalViewModel(){
         this.width = ko.observable(400);
@@ -86,10 +40,6 @@ define(function(require, exports, module){
             return true;
         }
 
-        this.onUrlCopy = _.bind(function(model, event){
-            nodeConnection.domains.clipboard.callCopy(this.url());
-        }, this);
-
         this.onUrlInsert = _.bind(function(model, event){
             var currentDoc = DocumentManager.getCurrentDocument(),
                 editor = EditorManager.getCurrentFullEditor(),
@@ -103,37 +53,6 @@ define(function(require, exports, module){
             editor.focus();
             editor.setSelection(pos, posEnd);
         }, this);
-
-        this.onFileSave = function(){
-            fs.showSaveDialog(function(opt, path){
-                var data = atob(getBase64Image($('.preview-box > img')[0]));
-
-                fs.saveFile(path, data, { }, function(err, stat, created){
-                    debugger;
-                });
-            });
-
-            event.stopPropagation();
-        }
-    }
-
-    function getBase64Image(img) {
-        // Create an empty canvas element
-        var canvas = document.createElement("canvas");
-        canvas.width = img.width;
-        canvas.height = img.height;
-
-        // Copy the image contents to the canvas
-        var ctx = canvas.getContext("2d");
-        ctx.drawImage(img, 0, 0);
-
-        // Get the data-URL formatted image
-        // Firefox supports PNG and JPEG. You could check img.src to guess the
-        // original format, but be aware the using "image/jpg" will re-encode the image.
-        var dataURL = canvas.toDataURL("image/png");
-
-        return dataURL.replace(/^data:image\/(png|jpg);base64,/, "");
-    }
 
     module.exports = ModalViewModel;
 });
